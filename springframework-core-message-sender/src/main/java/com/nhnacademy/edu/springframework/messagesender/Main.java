8d9a83dd62558bd3969b71b570c4b86b9a66e6ee
dduneon <dduneon@gmail.com>
2024-01-02 14:18:04 +0900

    ioc-1

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/Main.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/Main.java
@@ -1,11 +1,22 @@
 package com.nhnacademy.edu.springframework.messagesender;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
 
   public static void main(String[] args) {
     User user = new User("test@nhnacademy.com", "010-9465-3421");
 
-    MessageSendService smsMessageSendService = new MessageSendService(new SmsMessageSender());
-    smsMessageSendService.doSendMessage(user, "Hello");
+    try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml")) {
+      MessageSender emailMessageSender = context.getBean("emailMessageSender", MessageSender.class);
+      MessageSender smsMessageSender = context.getBean("smsMessageSender", MessageSender.class);
+      MessageSender emailMessageSender2 = context.getBean("emailMessageSender", MessageSender.class);
+
+      System.out.println(emailMessageSender == emailMessageSender2);
+      // singleton이기 때문에 두 객체는 같은 객체이다
+
+      emailMessageSender.sendMessage(user, "hello");
+      smsMessageSender.sendMessage(user, "hello");
+    }
   }
 }
