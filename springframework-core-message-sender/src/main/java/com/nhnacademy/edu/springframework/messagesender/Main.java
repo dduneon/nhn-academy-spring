919e6978f1eedec4af2e74f51cf05ebdeb44a3eb
dduneon <dduneon@gmail.com>
2024-01-02 11:34:21 +0900

    polymorphism-2

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/Main.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/Main.java
@@ -4,14 +4,8 @@ public class Main {
 
   public static void main(String[] args) {
     User user = new User("test@nhnacademy.com", "010-9465-3421");
-    sendSmsMessage(user, "hello world");
-  }
 
-  private static void sendSmsMessage(User user, String message) {
-    System.out.println("SMS Message Sent to " + user.getPhoneNumber() + " : " + message);
-  }
-
-  private static void sendEmailMessage(User user, String message) {
-    System.out.println("Email Message Sent to " + user.getEmail() + " : " + message);
+    MessageSendService smsMessageSendService = new MessageSendService(new SmsMessageSender());
+    smsMessageSendService.doSendMessage(user, "Hello");
   }
 }
