d0b90b1677730357e5254e5404bf3337808b6587
dduneon <dduneon@gmail.com>
2024-01-02 16:14:04 +0900

    di-1(setter injection)

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/service/MessageSendService.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/service/MessageSendService.java
@@ -6,6 +6,15 @@ import com.nhnacademy.edu.springframework.messagesender.User;
 public class MessageSendService {
   private MessageSender messageSender;
 
+  // setter injection 반드시 default constructor 필요
+  public MessageSendService() {
+  }
+
+  public void setMessageSender(
+      MessageSender messageSender) {
+    this.messageSender = messageSender;
+  }
+
   public MessageSendService(MessageSender messageSender) {
     this.messageSender = messageSender;
   }
