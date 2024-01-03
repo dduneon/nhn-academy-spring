915b2cc97d776560c9f4753115cbec481d8c095c
dduneon <dduneon@gmail.com>
2024-01-03 12:59:08 +0900

    java configuration-AnnotationConfigApplicationContext

---
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/JavaConfigMain.java
@@ -0,0 +1,16 @@
+package com.nhnacademy.edu.springframework.messagesender;
+
+import com.nhnacademy.edu.springframework.messagesender.service.MessageSendService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class JavaConfigMain {
+
+  public static void main(String[] args) {
+    User user = new User("test@nhnacademy.com", "010-9465-3421");
+
+    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("com.nhnacademy.edu.springframework.messagesender");
+    MessageSendService messageSendService = context.getBean("messageSendService", MessageSendService.class);
+    messageSendService.doSendMessage(user, "message");
+  }
+}
